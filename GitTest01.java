5901d9d85e240146ef041b56a1b98e04e397d47b
Faker <2499146889@qq.com>
2022-08-07 13:52:49 +0800

    GitTest first commit

---
+++ GitTest01.java
@@ -0,0 +1,9 @@
+public class GitTest01{
+	public static void main(String[] args){
+		System.out.println("Hello Git!");
+		System.out.println("Hello Git!");
+		System.out.println("Hello Git!");
+		System.out.println("Hello Git!");
+		System.out.println("Hello Git!");
+	}
+}
